9cc42585eb8cb20f3df62722aa82319ac09eee0a
evgeniypavlyukov <evgeniypavlyukov@gmail.com>
2022-11-14 20:24:26 +0200

    Add Constraints for DetailView

--- MVP разбор/DetailModule/View/DetailViewController.swift
+++ MVP разбор/DetailModule/View/DetailViewController.swift
@@ -19,11 +19,81 @@ class DetailViewController: UIViewController {
         view.backgroundColor = .white
         createLabelComment()
         createButtonPop()
-//        constraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        labelComment.translatesAutoresizingMaskIntoConstraints = false
+        buttonPop.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint(item: labelComment,
+                           attribute: .topMargin,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .topMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: labelComment,
+                           attribute: .bottomMargin,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .bottomMargin,
+                           multiplier: 1,
+                           constant: -200).isActive = true
+        
+        NSLayoutConstraint(item: labelComment,
+                           attribute: .leadingMargin,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .leadingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: labelComment,
+                           attribute: .trailingMargin,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .trailingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: buttonPop,
+                           attribute: .bottomMargin,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .bottomMargin,
+                           multiplier: 1,
+                           constant: -60).isActive = true
+        
+        NSLayoutConstraint(item: buttonPop,
+                           attribute: .topMargin,
+                           relatedBy: .equal,
+                           toItem: labelComment,
+                           attribute: .bottomMargin,
+                           multiplier: 1,
+                           constant: 20).isActive = true
+        
+        NSLayoutConstraint(item: buttonPop,
+                           attribute: .leadingMargin,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .leadingMargin,
+                           multiplier: 1,
+                           constant: 40).isActive = true
+        
+        NSLayoutConstraint(item: buttonPop,
+                           attribute: .trailingMargin,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .trailingMargin,
+                           multiplier: 1,
+                           constant: -40).isActive = true
     }
 
     func createLabelComment() {
-        labelComment = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+//        labelComment = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 500))
+        labelComment = UILabel(frame: .zero)
         labelComment.textAlignment = .center
         labelComment.numberOfLines = 0
         view.addSubview(labelComment)
@@ -32,8 +102,8 @@ class DetailViewController: UIViewController {
     
     func createButtonPop() {
         buttonPop = UIButton(type: .roundedRect)
-        buttonPop.frame = CGRect(origin: CGPoint(x: 0, y: 550), size: CGSize(width: 100, height: 100))
-        buttonPop.center.x = view.center.x
+//        buttonPop.frame = CGRect(origin: CGPoint(x: 0, y: 550), size: CGSize(width: 100, height: 100))
+//        buttonPop.center.x = view.center.x
         buttonPop.backgroundColor = .blue
         buttonPop.addTarget(self, action: #selector(tapButton), for: .touchDown)
         view.addSubview(buttonPop)
@@ -42,80 +112,6 @@ class DetailViewController: UIViewController {
     @objc func tapButton(){
         presenter.tap()
     }
-    
-    
-//    func constraints() {
-//        labelComment.translatesAutoresizingMaskIntoConstraints = false
-//        buttonPop.translatesAutoresizingMaskIntoConstraints = false
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//
-//
-//        NSLayoutConstraint(item: labelComment,
-//                           attribute: .topMargin,
-//                           relatedBy: .equal,
-//                           toItem: view,
-//                           attribute: .topMargin,
-//                           multiplier: 1,
-//                           constant: 0).isActive = true
-//
-//        NSLayoutConstraint(item: labelComment,
-//                           attribute: .bottomMargin,
-//                           relatedBy: .equal,
-//                           toItem: view,
-//                           attribute: .bottomMargin,
-//                           multiplier: 1,
-//                           constant: -200).isActive = true
-//
-//        NSLayoutConstraint(item: labelComment,
-//                           attribute: .leadingMargin,
-//                           relatedBy: .equal,
-//                           toItem: view,
-//                           attribute: .leadingMargin,
-//                           multiplier: 1,
-//                           constant: 0).isActive = true
-//
-//        NSLayoutConstraint(item: labelComment,
-//                           attribute: .trailingMargin,
-//                           relatedBy: .equal,
-//                           toItem: view,
-//                           attribute: .trailingMargin,
-//                           multiplier: 1,
-//                           constant: 0).isActive = true
-//
-//        NSLayoutConstraint(item: buttonPop,
-//                           attribute: .bottomMargin,
-//                           relatedBy: .equal,
-//                           toItem: view,
-//                           attribute: .bottomMargin,
-//                           multiplier: 1,
-//                           constant: 60).isActive = true
-//
-//        NSLayoutConstraint(item: buttonPop,
-//                           attribute: .topMargin,
-//                           relatedBy: .equal,
-//                           toItem: labelComment,
-//                           attribute: .bottomMargin,
-//                           multiplier: 1,
-//                           constant: 20).isActive = true
-//
-//        NSLayoutConstraint(item: buttonPop,
-//                           attribute: .leadingMargin,
-//                           relatedBy: .equal,
-//                           toItem: view,
-//                           attribute: .leadingMargin,
-//                           multiplier: 1,
-//                           constant: 40).isActive = true
-//
-//        NSLayoutConstraint(item: buttonPop,
-//                           attribute: .trailingMargin,
-//                           relatedBy: .equal,
-//                           toItem: view,
-//                           attribute: .trailingMargin,
-//                           multiplier: 1,
-//                           constant: 40).isActive = true
-//
-//
-//    }
     
 }
 
